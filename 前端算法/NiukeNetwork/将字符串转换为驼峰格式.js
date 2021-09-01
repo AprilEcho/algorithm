61bca0839f5742af9fa1a323b66f84adfa72f738
AprilEcho <8880617+aprilecho@user.noreply.gitee.com>
2021-09-01 19:45:14 +0800

    Update

--- 前端算法/NiukeNetwork/将字符串转换为驼峰格式.js
+++ 前端算法/NiukeNetwork/将字符串转换为驼峰格式.js
@@ -1,10 +1,10 @@
 function cssStyle2DomStyle(sName) {
   var strs = sName.split('-');
-  if(strs[0] == ""){
+  if (strs[0] == "") {
     var strs = sName.slice(1).split('-');
   }
-  for(var i = 1;i < strs.length; i++){
-    strs[i] = strs[i].charAt(0).toUpperCase()+strs[i].slice(1);
+  for (var i = 1; i < strs.length; i++) {
+    strs[i] = strs[i].charAt(0).toUpperCase() + strs[i].slice(1);
   }
   return strs.join('')
 }
